1bfbd3108bab80ba4aed1529a08175a6c5890370
raj balasubramanian <raj@rajs-MacBook-Pro-2.local>
2014-11-25 22:54:44 -0600

    updated fsm-2

--- fsm-2.py
+++ fsm-2.py
@@ -7,11 +7,16 @@ def getStr(l,ty):
  else:
   return ""
 
-def changeState2(startState,initState,finalState,trigger,l):
- if len(startState)>0:
-//  if (l.find(trigger) >-1) and (state==startStat
- else:
-//
+def changeState2(fsm,curState,l):
+ startState = fsm['startState']
+ trans = fsm['transitions']
+ val = (curState,"")
+ for key in trans.keys(): 
+  #print key,curState
+  if (l.find(key) >-1) and (curState in trans[key]['prevStates']):
+    val=(trans[key]['state'],getStr(l,key))
+ return val 
+
 
 def changeState(state,l):
  if (l.find('"deviceId":') >-1):
@@ -20,24 +25,29 @@ def changeState(state,l):
   return ("seen_model",getStr(l,'"model":'))
  elif (l.find('"systemVersion":')>-1) and (state=="seen_model"):
   return ("seen_sys",getStr(l,'"systemVersion":'))
- else
+ else:
   return (state,"")
 
 def process(foo):
- fsm={'triggers':('"deviceId":','"model":','"systemVersion":'),'states':('seen_device','seen_model','seen_sys'),'transitions':({'initState':"",'trigger':'"deviceId":','finalState':'seen_device'},{'initState':'seen_device','trigger':'"model":','finalState':'seen_model'},{'initState':'seen_model','trigger':'"systemVersion":','finalState':'seen_sys'})}
+ #fsm={'startState':'seen_device','triggers':('"deviceId":','"model":','"systemVersion":'),'states':('seen_device','seen_model','seen_sys'),'transitions':({'initState':"",'trigger':'"deviceId":','finalState':'seen_device'},{'initState':'seen_device','trigger':'"model":','finalState':'seen_model'},{'initState':'seen_model','trigger':'"systemVersion":','finalState':'seen_sys'})}
+
+ transitions={'"deviceId":':{'prevStates':("","seen_sys"),'state':"seen_device"},'"model":':{'prevStates':("seen_device"),'state':"seen_model"},'"systemVersion":':{'prevStates':("seen_model"),"state":"seen_sys"}}
+ states = ("seen_device","seen_sys","seen_model")
+ fsm={'startState':"seen_device","transitions":transitions,'endState':"seen_sys"}
  gstate=""
  tp=[]
  for l in open(foo,'r'):
-  gstate,str = changeState(gstate,l)
-  if (gstate=="seen_device" and len(str)>1):
+  gstate,str = changeState2(fsm,gstate,l)
+  if (gstate==fsm['startState']) and (len(str)>1):
    a=[]
    a.append(str)
-  elif (gstate=="seen_model" and len(str)>1):
+  elif (gstate in states) and (len(str)>1):
    a.append(str)
-  elif (gstate=="seen_sys" and len(str)>1):
-   a.append(str)
-   tp.append(a)
+  elif (gstate==fsm['endState']):
+   if len(a) > 1: 
+    tp.append(a)
+   a=[]
  return tp 
 
-print process("../19629.057.649.SystemOut.log.SOC.20140929")
+print process("testdata")
 
